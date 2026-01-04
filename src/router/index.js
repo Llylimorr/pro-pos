e20012d361f8e819828d18af11f73a611bf04eb8
Seng Pricha <prichaseng8@gmail.com>
2026-01-04 10:05:03 +0700

    update product01

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
+import Product from '../views/Products.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Dashboard',
     component: Dashboard
+  },
+  {
+    path: '/products',
+    name: 'Product',
+    component: Product
   }
 ]
 const router = createRouter({
